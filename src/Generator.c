eba8db1a85e5e54d1c17532573ae4c3234b69b09
Muesli_Reep <project_bravo@hotmail.com>
2015-12-19 14:43:27 +0100

    Generator stub

--- src/Generator.c
+++ src/Generator.c
@@ -18,3 +18,17 @@ Data to be generated and their update rate
 int generateTelemetry() {
  return 0;   
 }
+
+void generatePosition() {
+	int32_t lat = 0;
+	int32_t lon = 0;
+	int32_t alt = 0;
+	int32_t ralt = 0;
+
+	int16_t vx = 0;
+	int16_t vy = 0;
+	int16_t vz = 0;
+
+	uint16_t hdg = 0;
+
+}
